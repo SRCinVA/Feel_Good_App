25c0d227886e1f3cbe2f940e86cb0ffb6df8f5c6
SRCinVA <scitarella@hotmail.com>
2022-01-30 19:45:13 -0500

    Still problems, but pressing ahead
    Terminal commands seem to work, but no resolution of import issues in VSCode. Will push forward anyway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import Screen, ScreenManager
 
+Builder.load_file('design.kv')
