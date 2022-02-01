9da15f0f89c092150a67656cd338f576fec3b0c6
SRCinVA <scitarella@hotmail.com>
2022-01-31 20:20:22 -0500

    Most of kivy file complete ... ?

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 
 Builder.load_file('design.kv')
+
+# now we create two empty classes with the same names (see below) 
+class LoginScreen(Screen):
+# will inherit from screen object
+    pass
+class RootWidget(ScreenManager):
+    pass
