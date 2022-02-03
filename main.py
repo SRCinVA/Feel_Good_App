a78423a9f2f366491a5c54bf8767fab9e3365d64
SRCinVA <scitarella@hotmail.com>
2022-02-02 22:04:01 -0500

    Looks better

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 Builder.load_file('design.kv')
 
 # now we create two empty classes with the same names (see below) 
-class LoginScreen(Screen):
+class LoginScreen(Screen):  # under the RootWidget in the hierarchy
 # will inherit from screen object
     pass
-class RootWidget(ScreenManager):
+class RootWidget(ScreenManager):  # the next in the hierarchy
     pass
 
-class MainApp(App):   # the app object you haven't used yet (inherits from App above)
+class MainApp(App):   # the app object you haven't used yet (inherits from App above). It is the highest in the heirarchy.
     def build(self): # def build is from App.
         return RootWidget() # make sure it's the object and not the class (the brackets () initialize it.)
 
