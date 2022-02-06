12d23d77753f442fd0e1b2edbe35f0c47cc17830
SRCinVA <scitarella@hotmail.com>
2022-02-06 15:28:09 -0500

    Submit button basically functions

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Builder.load_file('design.kv')
 # now we create two empty classes with the same names (see below) 
 class LoginScreen(Screen):  # under the RootWidget in the hierarchy
 # will inherit from screen object
-    def sign_up(self):
+    def sign_up(self): # you can call these parameters anything; they are not related to the ids in the kivy file.
         self.manager.current = "sign_up_screen"
         # self refers to the instance of the LoginScreen object.
         # manager is a property of the parent 'screen'. Self (the instance) is able to access the 'manager' property
@@ -17,8 +17,8 @@ class RootWidget(ScreenManager):  # the next in the hierarchy
     pass
 
 class SignUpScreen(Screen):
-    def add_user(self): # this will send the user input to a json file and store then there.
-        pass
+    def add_user(self, uname, pword): # this will send the user input to a json file and store then there.
+        print(uname, pword)
 
 class MainApp(App):   # the app object you haven't used yet (inherits from App above). It is the highest in the heirarchy.
     def build(self): # def build is from App.
