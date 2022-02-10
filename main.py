13915f98bbf2dcf78d50fdcaf0a07f327c9686be
SRCinVA <scitarella@hotmail.com>
2022-02-09 22:20:50 -0500

    Started to build success screen

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ class LoginScreen(Screen):  # under the RootWidget in the hierarchy
 # will inherit from screen object
     def sign_up(self): # you can call these parameters anything; they are not related to the ids in the kivy file.
         self.manager.current = "sign_up_screen"
-        # self refers to the instance of the LoginScreen object.
-        # manager is a property of the parent 'screen'. Self (the instance) is able to access the 'manager' property
-        # current is an attribute of manager, and it will get the name of the scrren you want to switch to (in this case, sign_up_screen)
+        # 'self' refers to the instance of the LoginScreen object.
+        # 'manager' is a property of the parent 'screen'. Self (the instance) is able to access the 'manager' property.
+        # 'current' is an attribute of 'manager', and it will get the name of the scrren you want to switch to (in this case, sign_up_screen)
 
 class RootWidget(ScreenManager):  # the next in the hierarchy
     pass
@@ -32,7 +32,13 @@ class SignUpScreen(Screen):
         with open("users.json","w") as file:
             json.dump(users, file)  # we "dump" the users in the dictionary (or list?) to the .json file
                                     # it will overwrite the last dictionary but not "wipe" the content. (you can see the updated .json file)
+        self.manager.current = "sign_up_screen_success"
 
+class SignUpScreenSuccess(Screen): # it inherits from 'screen'
+
+    def back_to_login(self):
+        if True:
+            self.manager.current = "LoginScreen"
 
 class MainApp(App):   # the app object you haven't used yet (inherits from App above). It is the highest in the heirarchy.
     def build(self): # def build is from App.
